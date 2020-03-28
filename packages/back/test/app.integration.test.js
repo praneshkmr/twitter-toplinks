cb43142eb80da216446f96726f844da041f9d86c
Pranesh <pranesh@seniorly.com>
2020-03-28 17:54:24 +0530

    Backend ES lint setip

--- packages/back/test/app.integration.test.js
+++ packages/back/test/app.integration.test.js
@@ -1,10 +1,10 @@
-import request from "supertest";
-import app from "../src/app";
+import request from 'supertest';
+import app from '../src/app';
 
-describe("app", () => {
+describe('app', () => {
   it('GETs / and should obtain { foo: "bar" }', async () => {
     expect.assertions(1);
-    const res = await request(app).get("/").expect(200);
+    const res = await request(app).get('/').expect(200);
     expect(res.body).toMatchInlineSnapshot(`
       Object {
         "foo": "bar",
