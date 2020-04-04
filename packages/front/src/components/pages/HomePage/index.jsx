eec9177880cfd53af7de04b10166eff4e2023191
Pranesh <pranesh@seniorly.com>
2020-04-04 17:17:09 +0530

    Home page with Material UI

--- packages/front/src/components/pages/HomePage/index.jsx
+++ packages/front/src/components/pages/HomePage/index.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import { shape, func, string } from 'prop-types';
-import styled from 'styled-components';
+import { styled, makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
 
-const ClickSpan = styled.span`
-  font-weight: bold;
-`;
+import AppTemplate from '../../templates/AppTemplate';
 
-const HomePage = ({ user, twitterLogin }) => (
-  <>
-    <div>Home Page</div>
-    {!user && (
-    <div>
-      Click
-      <ClickSpan onClick={twitterLogin}>Here</ClickSpan>
-      {' '}
-      to login
-    </div>
-    )}
-    {user && <div>User Present</div>}
-  </>
-);
+const useStyles = makeStyles({
+  wrapper: {
+    display: 'flex',
+    'flex-direction': 'column',
+  },
+  heading: {
+    textAlign: 'center',
+    'margin-bottom': '24px',
+  },
+});
+
+const MyButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  margin: '0 auto',
+});
+
+const HomePage = ({ user, twitterLogin }) => {
+  const classes = useStyles();
+  return (
+    <AppTemplate>
+      <div className={classes.wrapper}>
+        <Typography variant="h3" className={classes.heading}>Welcome to Twitter TopLinks</Typography>
+        {!user && <MyButton onClick={twitterLogin}>Login with Twitter</MyButton>}
+        {user && <div>User Present</div>}
+      </div>
+    </AppTemplate>
+  );
+};
 
 HomePage.propTypes = {
   user: shape({ name: string }),
