61272509bd8bbd6ec624dcba25e4eefc7743e0fd
Pranesh <pranesh@seniorly.com>
2020-04-05 21:05:19 +0530

    Callback to mongoose connect

--- packages/back/src/mongoose.js
+++ packages/back/src/mongoose.js
@@ -6,9 +6,14 @@ import './models/userTweets';
 import './models/mostSharedLinks';
 
 export const StartDev = () => {
-  const URL = process.env.MONGO_URL || 'mongodb://localhost/test';
-  mongoose.connect(URL);
-  return mongoose;
+  const URL = process.env.MONGO_URL || 'mongodb://localhost/twitter-toplinks';
+  mongoose.connect(URL, { useNewUrlParser: true }, (err) => {
+    if (err) {
+      console.log('Error connecting to mongodb', err);
+    } else {
+      console.log('Successfully connected to MongoDB');
+    }
+  });
 };
 
 
